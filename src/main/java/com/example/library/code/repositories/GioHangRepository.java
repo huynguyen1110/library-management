d4c24a57bcbfc7d96293765fa9b41eab48b56b3a
huy nguyen <huynn@hivetech.vn>
2023-05-15 22:28:34 +0700

    api single product

---
+++ src/main/java/com/example/library/code/repositories/GioHangRepository.java
@@ -0,0 +1,9 @@
+package com.example.library.code.repositories;
+
+import com.example.library.code.models.entities.GioHang;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GioHangRepository extends JpaRepository<GioHang, Integer> {
+
+
+}
